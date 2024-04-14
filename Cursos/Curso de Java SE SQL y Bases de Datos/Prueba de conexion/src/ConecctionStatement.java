33191b1b383275d9caeaffca89647bc829fb1f2e
karyjb <kary.jimenez1127@gamil.com>
2024-04-14 12:26:52 -0500

    clases Statement y PreparedStatement

--- Cursos/Curso de Java SE SQL y Bases de Datos/Prueba de conexion/src/ConecctionStatement.java
+++ Cursos/Curso de Java SE SQL y Bases de Datos/Prueba de conexion/src/ConecctionStatement.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class ConecctionStatement {
-}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class ConecctionStatement {
+
+
+    //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+        public static void main(String[] args) {
+
+            Connection myConn = null;
+            Statement myStamt = null;
+            ResultSet myRes = null;
+
+
+            String url = "jdbc:mysql://localhost:3306/project";
+            String user = "root";
+            String password = "toor";
+
+            try {
+                myConn = DriverManager.getConnection(url, user, password);
+                System.out.println("Genial, nos conectamos!!!");
+
+
+                myStamt = myConn.createStatement();
+                myRes = myStamt.executeQuery("SELECT * FROM employees");
+
+
+              while (myRes.next()){
+                    System.out.println(myRes.getString("FIRST_NAME"));
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+                System.out.println("Algo salio mal :( ");
+            }
+        }
+    }
+
--- Cursos/Curso de Java SE SQL y Bases de Datos/Prueba de conexion/src/ConectionPreparedStatement.java
+++ Cursos/Curso de Java SE SQL y Bases de Datos/Prueba de conexion/src/ConectionPreparedStatement.java
@@ -2,11 +2,11 @@ import java.sql.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class ConectionPreparedStatement {
+
     public static void main(String[] args) {
 
         Connection myConn = null;
-        //Statement myStamt = null;
         PreparedStatement myStamt = null;
 
 
